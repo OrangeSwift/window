abdfc85326690f41a6d549bcc9e943500507a01f
OrangeSwift <67804052+OrangeSwift@users.noreply.github.com>
2022-01-20 10:01:37 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+#SDG
+#20/01/2022
+
+from tkinter import *
+
+root=Tk()
+root.title('Login')
+root.geometery('500x500')
+
+
+class winLog1:
+  def __init__(self, username, password, frame, button, smallFrame):
+    
+    self.username = username
+    self.password = password
+    self.frame = frame
+    self.button = button
+    self.smallFrame = smallFrame
+    
+    
+    
+    root.mainloop()
+    
+    
+  
+  
+               
+               
+               
+               
+               
+               
+  
